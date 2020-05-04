6b7c22041b1022f3d9080d03cc8b98119d835c82
Matt Osterfield <60369843+OstyPowers@users.noreply.github.com>
2020-05-04 15:38:42 +1200

    Start of Json that contains the hand rankings and the rankings of holecards

---
+++ Controller.js
@@ -0,0 +1,223 @@
+class Controller {
+    static getJSON () {
+        return {
+            "handrank": [
+            {
+                "rank": 1,
+                "name": "royal flush",
+            },
+            {
+                "rank": 2,
+                "name": "straight flush",
+            },
+            {
+                "rank": 3,
+                "name": "four of a kind",
+            },
+            {
+                "rank": 4,
+                "name": "full house",
+            },
+            {
+                "rank": 5,
+                "name": "flush",
+            },
+            {
+                "rank": 6,
+                "name": "straight",
+            }
+            {
+                "rank": 7,
+                "name": "three of a kind",
+            },
+            {
+                "rank": 8,
+                "name": "two pair",
+            },
+            {
+                "rank": 9,
+                "name": "pair",
+            },
+            {
+                "rank" 10,
+                "name": "high card",
+            },
+            ],
+            "rankorder": [
+            {
+                "rank": 1,
+                "holecards": [ace, ace],
+                "suited": false,
+            },
+            {
+                "rank": 2,
+                "holecards": [king, king],
+                "suited": false,
+            }
+            {
+                "rank": 3,
+                "holecars": [queen, queen],
+                "suited": false,
+            },
+            {
+                "rank": 4,
+                "holecards": [jack, jack],
+                "suited": false,
+            },
+            {
+                "rank": 5,
+                "holecards": [ace, king],
+                "suited": true,
+            },
+            {
+                "rank": 6,
+                "holecards": [ten, ten],
+                "suited": false,
+            },
+            {
+                "rank": 7,
+                "holecards": [ace, king],
+                "suited": false,
+            },
+            {
+                "rank": 8,
+                "holecards": [nine, nine],
+                "suited": false,
+            },
+            {
+                "rank": 9,
+                "holecards": [ace, queen],
+                "suited": true,
+            }
+            {
+                "rank": 10,
+                "holecards": [ace, queen],
+                "suited": false,
+            },
+            {
+                "rank": 11,
+                "holecards": [ace, jack],
+                "suited": true,
+            },
+            {
+                "rank": 12,
+                "holecards": [king, queen],
+                "suited": true,
+            },
+            {
+                "rank": 13,
+                "holecards": [ace, ten],
+                "suited": true,
+            },
+            {
+                "rank": 14,
+                "holecards": [queen, jack],
+                "suited": true,
+            },
+            {
+                "rank": 15,
+                "holecards": [king, jack],
+                "suited": true,
+            },
+            {
+                "rank": 16,
+                "holecards": [king, ten],
+                "suited": true,
+            },
+            {
+                "rank": 17,
+                "holecards": [eight, eight],
+                "suited": false,
+            },
+            {
+                "rank": 18,
+                "holecards": [jack, ten],
+                "suited": true,
+            },
+            {
+                "rank": 19,
+                "holecards": [queen, ten],
+                "suited": true,
+            },
+            {
+                "rank": 20,
+                "holecards": [seven, seven],
+                "suited": false,
+            },
+            {
+                "rank": 21,
+                "holecards": [six, six],
+                "suited": false,
+            },
+            {
+                "rank": 22,
+                "holecards": [ace, jack],
+                "suited": false,
+            },
+            {
+                "rank": 23,
+                "holecards": [ace, nine],
+                "suited": true,
+            },
+            {
+                "rank": 24,
+                "holecards": [king, queen],
+                "suited": false,
+            },
+            {
+                "rank": 25,
+                "holecards": [five, five],
+                "suited": false,
+            },
+            {
+                "rank": 26,
+                "holecards": [king, nine],
+                "suited": true,
+            },
+            {
+                "rank": 27,
+                "holecards": [ten, nine],
+                "suited": true,
+            },
+            {
+                "rank": 28,
+                "holecards": [jack, nine],
+                "suited": true,
+            },
+            {
+                "rank": 29,
+                "holecards": [queen, nine],
+                "suited": true,
+            },
+            {
+                "rank": 30,
+                "holecards": [four, four],
+                "suited": false,
+            },
+            {
+                "rank": 31,
+                "holecards": [ace, eight],
+                "suited": true,
+            },
+            {
+                "rank": 32,
+                "holecards": [ace, ten],
+                "suited": false,
+            },
+            {
+                "rank": 33,
+                "holecards": [three, three],
+                "suited": false,
+            },
+            {
+                "rank": 34,
+                "holecards": [ace, seven],
+                "suited": true,
+            },
+            {
+                "rank": 35,
+                "holecards":
+            }]
+        }
+    }
+}
