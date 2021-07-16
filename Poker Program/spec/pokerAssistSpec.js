b98b17bfdc3533fa92bb26dfb59b3cab2d2ec6b5
Matt Osterfield <60369843+OstyPowers@users.noreply.github.com>
2021-07-16 21:31:37 +1200

    Added more tests

--- Poker Program/spec/pokerAssistSpec.js
+++ Poker Program/spec/pokerAssistSpec.js
@@ -1,5 +1,3 @@
-/* globals describe it xdescribe xit beforeEach expect Ballot localStorage STORAGE_KEY */
-
 describe('Poker Assist', function() {
 
   var theHand;
@@ -36,5 +34,74 @@ describe('Poker Assist', function() {
             });
         
         });
+    });
+
+    describe('Testing User Input', function() {
+
+        describe('when the user inputs ace ace with a position of button', function() {
+            beforeEach(function() {
+                theHand.addHoleCardA('ace');
+                theHand.addHoleCardB('ace');
+                theHand.suited(false)
+                theHand.addPosition('Button');
+            });
+
+            describe('the card rank', function() {
+                it('should be 1, 1', function() {
+                    expect(theHand.getCardRank).toBe(1,1);
+                });
+            });
+
+            describe('the hand rank', function() {
+                it('should be 1', function() {
+                    expect(theHand.getHandRank).toBe(1);
+                });
+            });
+
+            describe('the position', function() {
+                it('should be "Button"', function() {
+                    expect(theHand.getPosition).toBe('Button');
+                });
+            });
+
+            describe('the hand value', function() {
+                it('should be "pair"', function() {
+                    expect(theHand.getHandValue).toBe('pair');
+                });
+            });
+        });
+
+        describe('when the user inputs jack ten with a position of small blind', function() {
+            beforeEach(function() {
+                theHand.addHoleCardA('jack');
+                theHand.addHoleCardB('ten');
+                theHand.suited(true);
+                theHand.addPosition('Small Blind');
+                });
+            
+                describe('the card rank', function() {
+                    it('should be 4, 5', function() {
+                        expect(theHand.getCardRank).toBe(4,5);
+                    });
+                });
+
+                describe('the hand rank', function() {
+                    it('should be 18', function() {
+                        expect(theHand.getHandRank).toBe(18);
+                    });
+                });
+
+                describe('the position', function() {
+                    it('should be "Small Blind', function() {
+                        expect(theHand.getPosition).toBe('Small Blind');
+                    });
+                });
+
+                describe('the hand value', function() {
+                    it('should be "high card"', function() {
+                        expect(theHand.getHandValue).toBe('high card');
+                    });
+                });
+            });
     });
 });
